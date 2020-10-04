569feea68b355ba791dce12b8f4712ca35955d8c
Lulzat <j.lulzat@gmail.com>
2020-10-03 23:03:22 -0500

    And the survey says..?!

--- src/views/Landing.vue
+++ src/views/Landing.vue
@@ -257,35 +257,38 @@
           <div class="flex flex-wrap justify-center lg:-mt-64 -mt-48">
             <div class="w-full lg:w-6/12 px-4">
               <div class="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded-lg bg-gray-300">
-                <form name="PrideForm" @submit.prevent="checkForm" data-netlify="true" class="flex-auto p-5 lg:p-10" id="myForm">
-                  <br/>
-                  <h4 class="text-2xl font-semibold">Let's Do This</h4>
-                  <p class="leading-relaxed mt-1 mb-4 text-gray-600">
-                    Send us a quick message, and let us know what we can help you with.
-                  </p>
+                <form name="PrideForm" id="myForm" method="post" data-netlify="true" enctype="application/x-www-form-urlencoded" @submit.prevent="checkForm" class="flex-auto p-5 lg:p-10">
+                  <div class="m-auto p-2">
+                    <h4 class="text-2xl font-semibold">
+                      Let's Do This
+                    </h4>
+                    <p class="leading-relaxed mt-1 mb-4 text-gray-600">
+                      Send us a quick message, and let us know what we can help you with.
+                    </p>
+                  </div>
                   <div class="relative w-full mb-3 mt-8">
-                    <label class="block uppercase text-gray-700 text-xs font-bold mb-2"
-                      for="full-name">Name</label><input type="text" name="name" v-model="formData.name"
-                      class="px-3 py-3 placeholder-gray-400 text-gray-700 bg-white rounded text-sm shadow focus:outline-none focus:shadow-outline w-full"
-                      placeholder="Full Name" style="transition: all 0.15s ease 0s;" />
+                    <label class="block uppercase text-gray-700 text-xs font-bold mb-2" for="full-name">
+                      Name
+                    </label>
+                    <input type="text" name="name" v-model="formData.name" class="px-3 py-3 placeholder-gray-400 text-gray-700 bg-white rounded text-sm shadow focus:outline-none focus:shadow-outline w-full" placeholder="Full Name" style="transition: all 0.15s ease 0s;" />
                   </div>
                   <div class="relative w-full mb-3">
-                    <label class="block uppercase text-gray-700 text-xs font-bold mb-2"
-                      for="email">Telephone</label><input type="tel" name="telephone" v-model="formData.telephone"
-                      class="px-3 py-3 placeholder-gray-400 text-gray-700 bg-white rounded text-sm shadow focus:outline-none focus:shadow-outline w-full"
-                      placeholder="Email" style="transition: all 0.15s ease 0s;" />
+                    <label class="block uppercase text-gray-700 text-xs font-bold mb-2" for="email">
+                      Telephone
+                    </label>
+                    <input type="tel" name="telephone" v-model="formData.telephone" class="px-3 py-3 placeholder-gray-400 text-gray-700 bg-white rounded text-sm shadow focus:outline-none focus:shadow-outline w-full" placeholder="Email" style="transition: all 0.15s ease 0s;" />
                   </div>
                   <div class="relative w-full mb-3">
-                    <label class="block uppercase text-gray-700 text-xs font-bold mb-2" for="email">Email</label><input
-                      type="email" name="email" v-model="formData.email"
-                      class="px-3 py-3 placeholder-gray-400 text-gray-700 bg-white rounded text-sm shadow focus:outline-none focus:shadow-outline w-full"
-                      placeholder="Email" style="transition: all 0.15s ease 0s;" />
+                    <label class="block uppercase text-gray-700 text-xs font-bold mb-2" for="email">
+                      Email
+                    </label>
+                    <input type="email" name="email" v-model="formData.email" class="px-3 py-3 placeholder-gray-400 text-gray-700 bg-white rounded text-sm shadow focus:outline-none focus:shadow-outline w-full" placeholder="Email" style="transition: all 0.15s ease 0s;" />
                   </div>
                   <div class="relative w-full mb-3">
-                    <label class="block uppercase text-gray-700 text-xs font-bold mb-2"
-                      for="message">Message</label><textarea name="message" rows="4" cols="80"
-                      class="px-3 py-3 placeholder-gray-400 text-gray-700 bg-white rounded text-sm shadow focus:outline-none focus:shadow-outline w-full"
-                      placeholder="Type a message..." v-model="formData.message"></textarea>
+                    <label class="block uppercase text-gray-700 text-xs font-bold mb-2" for="message">
+                      Message
+                    </label>
+                    <textarea name="message" rows="4" cols="80" class="px-3 py-3 placeholder-gray-400 text-gray-700 bg-white rounded text-sm shadow focus:outline-none focus:shadow-outline w-full" placeholder="Type a message..." v-model="formData.message"></textarea>
                   </div>
                   <div class="text-center mt-6">
                     <p v-if="errors.length" class="text-pride-red p-3">
@@ -296,7 +299,7 @@
                     </p>
                     <button
                       class="bg-gray-900 text-white active:bg-gray-700 text-sm font-bold uppercase px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1"
-                      type="submit" style="transition: all 0.15s ease 0s;">
+                      type="submit" name="button" style="transition: all 0.15s ease 0s;">
                       Send Message
                     </button>
                   </div>
@@ -363,7 +366,7 @@ export default {
           .catch(error => console.log(error))
           .then(document.getElementById("myForm").innerHTML = `
             <div>
-                Thank you! I received your submission.
+              Contact Form Submitted! Thank you!
             </div>
             `)
            e.preventDefault();
